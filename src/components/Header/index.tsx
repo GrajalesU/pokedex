91eb2ddc2aab8cc51b803c8e6bd8368c631f6b8a
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-13 17:36:27 -0500

    Fix header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import './styles.scss';
 
+import pokeball from '../../assets/pokeball.svg';
+
 const Header = () => {
   return (
     <header className="header">
       <div className="headerLogo">
-        <img src="src/assets/pokeball.svg" alt="pokeball" className="headerLogoImg" />
+        <img src={pokeball} alt="pokeball" className="headerLogoImg" />
         <span className="headerLogoName">POKÉDEX_</span>
       </div>
     </header>
